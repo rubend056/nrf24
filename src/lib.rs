861c1aeedbc9fba4dda13e2f3ef789e7782573fa
RubenD <rubendariopm14@gmail.com>
2024-11-22 17:32:30 -0500

    code working, few high level functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-mod device;
+mod registers;
 
 use core::fmt::Debug;
 
-use device::*;
+use registers::*;
 use embedded_hal::{
 	delay::DelayNs,
 	digital::OutputPin,
@@ -13,8 +13,9 @@ use embedded_hal::{
 
 use Error::Pin;
 #[derive(Debug)]
+#[repr(u8)]
 pub enum Error<SPIE, PINE> {
-	Spi(SPIE),
+	Spi(SPIE) = 10,
 	Pin(PINE),
 	// other custom errors
 	TxFifoFull,
@@ -22,7 +23,26 @@ pub enum Error<SPIE, PINE> {
 	NotInTxMode,
 	NotInRxMode,
 	PayloadWidthInvalid,
+	PipeInvalid,
+	AddrTooLong,
 }
+// So we can pull the u8 value out of it
+impl<SPIE, PINE> Error<SPIE, PINE> {
+	pub fn discriminant(&self) -> u8 {
+		// SAFETY: Because `Self` is marked `repr(u8)`, its layout is a `repr(C)` `union`
+		// between `repr(C)` structs, each of which has the `u8` discriminant as its first
+		// field, so we can read the discriminant without offsetting the pointer.
+		unsafe { *<*const _>::from(self).cast::<u8>() }
+	}
+}
+
+#[derive(Debug)]
+pub enum PayloadType {
+	Payload,
+	PayloadWithNoAck,
+	AckPayload,
+}
+use PayloadType::*;
 // We only implement From<SPIE> bc if PINE is implemented
 // compiler complains about multiple implementations.
 impl<SPIE, PINE> From<SPIE> for Error<SPIE, PINE> {
@@ -56,6 +76,8 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 	fn ce_disable(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
 		Ok(self.ce.set_low().map_err(Pin)?)
 	}
+
+	/// Executable in power down or standby modes only.
 	fn write_register<R: Register>(
 		&mut self,
 		register: R,
@@ -80,21 +102,74 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 		])?;
 		Ok(buf)
 	}
-	fn write_tx_payload(
+
+	fn write_payload(
 		&mut self,
 		buf: &[u8],
-		no_ack: bool,
+		payload_type: PayloadType,
 	) -> Result<(), Error<SPI::Error, CE::Error>> {
 		self.spi.transaction(&mut [
-			Operation::Write(&[if no_ack { W_TX_P_NO_ACK } else { W_TX_PAYLOAD }]),
+			Operation::Write(&[match payload_type {
+				Payload => W_TX_PAYLOAD,
+				PayloadWithNoAck => W_TX_P_NO_ACK,
+				AckPayload => W_ACK_PAYLOAD,
+			}]),
 			Operation::Write(buf),
 		])?;
 		Ok(())
 	}
-	fn flush_rx(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
+	/// Remember that addr should start with LSB first.
+	pub fn set_rx_addr(
+		&mut self,
+		pipe: u8,
+		addr: &[u8],
+	) -> Result<(), Error<SPI::Error, CE::Error>> {
+		let pipe_addr = match pipe {
+			0 => RX_ADDR_P0,
+			1 => RX_ADDR_P1,
+			2 => RX_ADDR_P2,
+			3 => RX_ADDR_P3,
+			4 => RX_ADDR_P4,
+			5 => RX_ADDR_P5,
+			_ => return Err(Error::PipeInvalid),
+		};
+		if pipe <= 1 {
+			if addr.len() > 5 {
+				return Err(Error::AddrTooLong);
+			}
+			let mut buf = [0u8; 6];
+			buf[0] = W_REGISTER | pipe_addr;
+			let buf_len = addr.len() + 1; // max of 6 (1 command byte, max 5 addr bytes)
+			buf[1..buf_len].copy_from_slice(addr);
+			self.spi.write(&buf[..buf_len])?;
+		} else {
+			if addr.len() > 1 {
+				return Err(Error::AddrTooLong);
+			}
+			let mut buf = [0u8; 2];
+			buf[0] = W_REGISTER | pipe_addr;
+			buf[1] = addr[0];
+			self.spi.write(&buf)?;
+		}
+
+		Ok(())
+	}
+	/// Remember that addr should start with LSB first.
+	pub fn set_tx_addr(&mut self, addr: &[u8]) -> Result<(), Error<SPI::Error, CE::Error>> {
+		if addr.len() > 5 {
+			Err(Error::AddrTooLong)
+		} else {
+			let mut buf = [0u8; 6];
+			buf[0] = W_REGISTER | TX_ADDR;
+			let buf_len = addr.len() + 1; // max of 6 (1 command byte, max 5 addr bytes)
+			buf[1..buf_len].copy_from_slice(addr);
+			Ok(self.spi.write(&buf[..buf_len])?)
+		}
+	}
+	pub fn flush_rx(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
 		Ok(self.spi.write(&[FLUX_RX])?)
 	}
-	fn flush_tx(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
+	pub fn flush_tx(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
 		Ok(self.spi.write(&[FLUX_TX])?)
 	}
 	fn reuse_tx_payload(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
@@ -152,17 +227,17 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 
 	// --------------- Functional Operations ---------------
 
-	/// If RX FIFO is not empty, return the pipe number
-	pub fn data_available(&mut self) -> Result<Option<u8>, Error<SPI::Error, CE::Error>> {
-		self.read_register::<FifoStatus>()
-			.map(|(status, fifo_status)| {
-				if !fifo_status.rx_empty() {
-					Some(status.rx_p_no())
-				} else {
-					None
-				}
-			})
-	}
+	// /// If RX FIFO is not empty, return the pipe number
+	// pub fn data_available(&mut self) -> Result<Option<u8>, Error<SPI::Error, CE::Error>> {
+	// 	self.read_register::<FifoStatus>()
+	// 		.map(|(status, fifo_status)| {
+	// 			if !fifo_status.rx_empty() {
+	// 				Some(status.rx_p_no())
+	// 			} else {
+	// 				None
+	// 			}
+	// 		})
+	// }
 
 	/// Perform initialization sequence
 	///
@@ -182,13 +257,108 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 		self.flush_tx()?;
 		Ok(())
 	}
+	
+}
+
+impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
+	/// All the things we can configure are:
+	///
+	/// - Config (masking IRQ flags, CRC, CRCO)
+	/// - EN_AA (enabling auto-acknowledgement)
+	/// - EN_RXADDR (enabling data pipes for RX)
+	/// - SETUP_AW (setup of address widths)
+	/// - SETUP_RETR (ard [retransmission delay], arc [retransmission count])
+	/// - RF_CH (rf channel)
+	/// - RF_SETUP (data rate, rf power)
+	/// - RX/TX_addr (addresses) & RX_PW (static # bytes in pipes)
+	/// - DYNPD (enable dynamic payloads for pipes)
+	/// - Feature
+	/// 	- enable dynamic payloads
+	/// 	- enable ack payloads
+	/// 	- enable dynamic ack (just enables the writing of a payloads with no ack apparently)
+	pub fn configure(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
+		let mut config = Config::default();
+		// Set crc of 2 bytes
+		config.set_crco(true);
+		// Won't mask any flags
+		self.write_register(config)?;
+
+		// en_aa is already set to true on all pipes, but we'll set it anyways
+		self.write_register(EnAa::from_bools(&[true, false, false, false, false, false]))?;
+
+		// only enable first pipe
+		self.write_register(EnRxaddr::from_bools(&[
+			true, false, false, false, false, false,
+		]))?;
+
+		let mut setup_aw = SetupAw::default();
+		setup_aw.set_aw(0b11); // 5 byte addresses
+		self.write_register(setup_aw)?;
+
+		let mut setup_retr = SetupRetr::default();
+		setup_retr.set_ard(5); // 1500 uS or 1.5ms
+		setup_retr.set_arc(3); // 3 retransmissions
+		self.write_register(setup_retr)?;
+
+		let mut rf_ch = RfCh::default();
+		rf_ch.set_rf_ch(24); // (2400 MHz + ch)
+		self.write_register(rf_ch)?;
+
+		let mut rf_setup = RfSetup::default();
+		rf_setup.set_rf_dr_low(true); // enable 250KHz data rate
+		rf_setup.set_rf_dr_high(false); // ik it's dont care, but for sake of it
+		rf_setup.set_rf_pwr(0b11); // 0dBm MAX power
+		self.write_register(rf_setup)?;
+
+		// We won't set addresses or static number of bytes as defaults are ok
+		// either way application should modify rx_addr and tx_addr themselves
+
+		// Enable dynamic payloads on first pipe
+		self.write_register(Dynpd::from_bools(&[
+			true, false, false, false, false, false,
+		]))?;
+
+		let mut feature = Feature::default();
+		feature.set_en_dpl(true); // Enable dynamic payloads
+		feature.set_en_ack_pay(false); // Disable acks with payloads
+		feature.set_en_dyn_ack(true); // Enable writing payloads with no-ack.
+		self.write_register(feature)?;
+
+		Ok(())
+	}
+
+	pub fn set_auto_ack_pipes(
+		&mut self,
+		pipes: &[bool; PIPES_COUNT],
+	) -> Result<(), Error<SPI::Error, CE::Error>> {
+		self.write_register(EnAa::from_bools(pipes))?;
+		Ok(())
+	}
+	pub fn set_rx_enabled_pipes(
+		&mut self,
+		pipes: &[bool; PIPES_COUNT],
+	) -> Result<(), Error<SPI::Error, CE::Error>> {
+		self.write_register(EnRxaddr::from_bools(pipes))?;
+		Ok(())
+	}
+	pub fn set_dynamic_payload_pipes(
+		&mut self,
+		pipes: &[bool; PIPES_COUNT],
+	) -> Result<(), Error<SPI::Error, CE::Error>> {
+		self.write_register(Dynpd::from_bools(pipes))?;
+		Ok(())
+	}
 
 
 	/// Start a transmission
+	///
+	/// If we're expecting an ack Shockburst,
+	/// then we need to set RX_ADDR_P0 same as TX_ADDR
+	/// this function currently doesn't do that.
 	pub fn transmission_start<T: DelayNs>(
 		&mut self,
 		payload: &[u8],
-		no_ack: bool,
+		payload_type: PayloadType,
 		delay: &mut T,
 	) -> Result<(), Error<SPI::Error, CE::Error>> {
 		if self.config.prim_rx() == true {
@@ -203,7 +373,7 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 			}
 		}
 		self.clear_interrupts(MAX_RT_FLAG | TX_DS_FLAG)?;
-		self.write_tx_payload(payload, no_ack)?;
+		self.write_payload(payload, payload_type)?;
 		self.ce_enable()?;
 		delay.delay_us(100);
 		self.ce_disable()?;
@@ -232,7 +402,7 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 	///
 	/// This function will error if there's no payload available
 	/// so make sure you only call this when you know there's a payload.
-	fn receive_raw(&mut self) -> Result<[u8; 32], Error<SPI::Error, CE::Error>> {
+	fn receive(&mut self) -> Result<[u8; 32], Error<SPI::Error, CE::Error>> {
 		let (_, payload_width) = self.read_rx_payload_width()?;
 		if payload_width > 32 || payload_width == 0 {
 			return Err(Error::PayloadWidthInvalid);
@@ -244,22 +414,35 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 	/// Receive a payload
 	///
 	/// This function will clear rx interrupt flags when no payload is available.
-	pub fn receive(&mut self) -> Result<Option<[u8; 32]>, Error<SPI::Error, CE::Error>> {
+	///
+	/// Returns Option<(pipe_number, payload)>
+	pub fn receive_maybe(
+		&mut self,
+	) -> Result<Option<(u8, [u8; 32])>, Error<SPI::Error, CE::Error>> {
 		if self.config.prim_rx() == false {
 			return Err(Error::NotInRxMode);
 		}
 
-		let (_, fifo) = self.read_register::<FifoStatus>()?;
-		if fifo.rx_full() {
-			// If fifo is full, then return error
-			Err(Error::RxFifoFull)
-		} else if fifo.rx_empty() {
+		let status;
+		if CHECK_FIFO_OVERFLOW {
+			let (status_, fifo) = self.read_register::<FifoStatus>()?;
+			status = status_;
+			if fifo.rx_full() {
+				// If fifo is full, then return error
+				return Err(Error::RxFifoFull);
+			}
+		} else {
+			status = self.nop()?;
+		}
+
+		if status.rx_p_no() == 0b111 {
 			// If fifo is empty, clear the rx interrupt
 			self.clear_interrupts(RX_DR_FLAG)?;
 			Ok(None)
 		} else {
 			// If fifo isn't empty or full then receive the data
-			self.receive_raw().map(|v| Some(v))
+			self.receive()
+				.map(|payload| Some((status.rx_p_no(), payload)))
 		}
 	}
 
@@ -275,11 +458,17 @@ impl<SPI: SpiDevice<u8>, CE: OutputPin> NRF24L01<SPI, CE> {
 	}
 	/// Switches to TX mode
 	///
-	/// disables ce first just in case we're still in rx;
-	/// keep in mind a transition to TX takes 130us of settling
+	/// keep in mind a transition to TX takes 130us of settling;
+	/// disables ce first just in case we're still in rx
 	pub fn tx(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
 		self.ce_disable()?;
 		self.set_prim_rx(false)?;
+		Ok(())
+	}
+
+	/// Disables CE
+	pub fn idle(&mut self) -> Result<(), Error<SPI::Error, CE::Error>> {
+		self.ce_disable()?;
 		Ok(())
 	}
 }
--- src/registers.rs
+++ src/registers.rs
@@ -67,7 +67,7 @@ macro_rules! register {
 	};
 }
 macro_rules! pipes_accessors {
-	($name: ident, $default: expr, $getter: ident, $setter: ident) => {
+	($name: ident, $getter: ident, $setter: ident) => {
 		impl $name {
 			#[inline]
 			pub fn $getter(&self, pipe_no: usize) -> bool {
@@ -86,7 +86,7 @@ macro_rules! pipes_accessors {
 			}
 
 			pub fn from_bools(bools: &[bool; PIPES_COUNT]) -> Self {
-				let mut register = $name($default);
+				let mut register = $name::default();
 				for (i, b) in bools.iter().enumerate() {
 					register.$setter(i, *b);
 				}
@@ -133,14 +133,14 @@ register!(Config, 0x00, 0b_0000_1000);
 /// Enable Auto Acknowledgment
 #[derive(Debug)]
 pub struct EnAa(pub u8);
-register!(EnAa, 0x01);
-pipes_accessors!(EnAa, 0b0011_1111, enaa_p, set_enaa_p);
+register!(EnAa, 0x01, 0b_0011_1111);
+pipes_accessors!(EnAa, enaa_p, set_enaa_p);
 
 /// Enabled RX Addresses
 #[derive(Debug)]
 pub struct EnRxaddr(u8);
-register!(EnRxaddr, 0x02);
-pipes_accessors!(EnRxaddr, 0b0000_0011, erx_p, set_erx_p);
+register!(EnRxaddr, 0x02, 0b_0000_0011);
+pipes_accessors!(EnRxaddr, erx_p, set_erx_p);
 
 bitfield! {
 	/// Setup Address Width
@@ -153,7 +153,7 @@ bitfield! {
 	/// * `0b11`: 5 bytes
 	pub u8, aw, set_aw: 1, 0;
 }
-register!(SetupAw, 0x03);
+register!(SetupAw, 0x03, 0b_0000_0011);
 
 bitfield! {
 	/// Setup of Automatic Retransmission
@@ -165,7 +165,7 @@ bitfield! {
 	/// Auto Retransmit Count
 	pub u8, arc, set_arc: 3, 0;
 }
-register!(SetupRetr, 0x04);
+register!(SetupRetr, 0x04, 0b_0000_0011);
 
 bitfield! {
 	/// RF Channel
@@ -176,7 +176,7 @@ bitfield! {
 	/// 7 bits 0-127
 	pub u8, rf_ch, set_rf_ch: 6, 0;
 }
-register!(RfCh, 0x05);
+register!(RfCh, 0x05, 0b0000_0010);
 
 bitfield! {
 	/// RF Setup
@@ -200,7 +200,7 @@ bitfield! {
 	/// * `11`: 0 dBm
 	pub u8, rf_pwr, set_rf_pwr: 2, 1;
 }
-register!(RfSetup, 0x06);
+register!(RfSetup, 0x06, 0b_0000_1110);
 
 
 /// Interrupt flags for status register
@@ -219,12 +219,16 @@ bitfield! {
 	pub tx_ds, set_tx_ds: 5;
 	/// Maximum number of TX retransmits interrupt. Write `true` to clear.
 	pub max_rt, set_max_rt: 4;
-	/// Data pipe number for reading from RX FIFO
+	/// Data pipe number for the payload available for reading from `RX_FIFO`
+	///
+	/// 000-101: Data Pipe Number
+	/// 110: Not used
+	/// 111: RX FIFO Empty
 	pub u8, rx_p_no, _: 3, 1;
 	/// TX FIFO full flag
 	pub tx_full, _: 0;
 }
-register!(Status, 0x07);
+register!(Status, 0x07, 0b_0000_1110);
 
 bitfield! {
 	/// Transmit observe register
@@ -239,7 +243,7 @@ bitfield! {
 	/// when transmission of a new packet starts.
 	pub u8, arc_cnt, _: 3, 0;
 }
-register!(ObserveTx, 0x08);
+register!(ObserveTx, 0x08, 0b_0000_0000);
 
 bitfield! {
 	/// Received Power Detect
@@ -254,12 +258,40 @@ bitfield! {
 	/// power level threshold for this bit.
 	pub rpd, _: 0;
 }
-register!(RPD, 0x09);
+register!(RPD, 0x09, 0b_0000_0000);
 
+/// Receive address data pipe 0 (5 bytes, LSB first)
+pub const RX_ADDR_P0: u8 = 0x0A;
+pub const RX_ADDR_P0_DEFAULT: [u8; 5] = [0xE7, 0xE7, 0xE7, 0xE7, 0xE7];
+/// Receive address data pipe 1 (5 bytes, LSB first)
+pub const RX_ADDR_P1: u8 = 0x0B;
+pub const RX_ADDR_P1_DEFAULT: [u8; 5] = [0xC2, 0xC2, 0xC2, 0xC2, 0xC2];
+/// Receive address data pipe 2 (1 byte LSB, rest is RX_ADDR_P1)
+pub const RX_ADDR_P2: u8 = 0x0C;
+pub const RX_ADDR_P2_DEFAULT: u8 = 0xC3;
+/// Receive address data pipe 3 (1 byte LSB, rest is RX_ADDR_P1)
+pub const RX_ADDR_P3: u8 = 0x0D;
+pub const RX_ADDR_P3_DEFAULT: u8 = 0xC4;
+/// Receive address data pipe 4 (1 byte LSB, rest is RX_ADDR_P1)
+pub const RX_ADDR_P4: u8 = 0x0E;
+pub const RX_ADDR_P4_DEFAULT: u8 = 0xC5;
+/// Receive address data pipe 5 (1 byte LSB, rest is RX_ADDR_P1)
+pub const RX_ADDR_P5: u8 = 0x0F;
+pub const RX_ADDR_P5_DEFAULT: u8 = 0xC6;
+/// Transmit adress. Used for a PTX device only. (LSB first)
+///
+/// Set RX_ADDR_P0 equal to this address to handle automatic
+/// acknowledge if this is a PTX device with Enhanced Shockburst enabled.
+pub const TX_ADDR: u8 = 0x10;
+pub const RX_ADDR_DEFAULT: [u8; 5] = RX_ADDR_P0_DEFAULT;
+
+// ----- Data Pipe Lengths -------
 macro_rules! def_rx_pw {
 	($name: ident, $addr: expr) => {
 		bitfield! {
 			/// Static payload length for RX
+			///
+			/// initialized to 0 by default
 			pub struct $name(u8);
 			impl Debug;
 
@@ -270,12 +302,14 @@ macro_rules! def_rx_pw {
 	};
 }
 
+
 def_rx_pw!(RxPwP0, 0x11);
 def_rx_pw!(RxPwP1, 0x12);
 def_rx_pw!(RxPwP2, 0x13);
 def_rx_pw!(RxPwP3, 0x14);
 def_rx_pw!(RxPwP4, 0x15);
 def_rx_pw!(RxPwP5, 0x16);
+// -------------------------------
 
 bitfield! {
 	/// FIFO Status Register
@@ -301,12 +335,12 @@ bitfield! {
 	/// RX FIFO empty flag
 	pub rx_empty, _: 0;
 }
-register!(FifoStatus, 0x17);
+register!(FifoStatus, 0x17, 0b_0001_0001);
 
 /// Enable Dynamic Payload length
 pub struct Dynpd(pub u8);
-register!(Dynpd, 0x1C);
-pipes_accessors!(Dynpd, 0, dpl_p, set_dpl_p);
+register!(Dynpd, 0x1C, 0b_0000_0000);
+pipes_accessors!(Dynpd, dpl_p, set_dpl_p);
 
 bitfield! {
 	/// Enable features
@@ -320,29 +354,146 @@ bitfield! {
 	/// Enables the W_TX_PAYLOAD_NOACK command
 	pub en_dyn_ack, set_en_dyn_ack: 0;
 }
-register!(Feature, 0x1D);
+register!(Feature, 0x1D, 0b_0000_0000);
+
 
 #[test]
-fn test_pipe_accessors() {
-	let mut en_rx_addr = EnRxaddr::from_bools(&[true, true, false, false, false, false]);
-	assert_eq!(en_rx_addr.0, 0b_0000_0011);
+fn pipe_accessors() {
+	let mut en_rx_addr = EnRxaddr::from_bools(&[true, true, false, true, false, false]);
+	assert_eq!(en_rx_addr.0, 0b_0000_1011);
 	assert_eq!(en_rx_addr.erx_p(0), true);
 	assert_eq!(en_rx_addr.erx_p(1), true);
 	assert_eq!(en_rx_addr.erx_p(2), false);
+	assert_eq!(en_rx_addr.erx_p(3), true);
 	assert_eq!(en_rx_addr.erx_p(5), false);
 	assert_eq!(en_rx_addr.erx_p(7), false);
 	assert_eq!(
 		en_rx_addr.to_bools(),
-		[true, true, false, false, false, false]
+		[true, true, false, true, false, false]
 	);
 	en_rx_addr.set_erx_p(2, true);
 	assert_eq!(en_rx_addr.erx_p(0), true);
 	assert_eq!(en_rx_addr.erx_p(1), true);
 	assert_eq!(en_rx_addr.erx_p(2), true);
+	assert_eq!(en_rx_addr.erx_p(3), true);
 	assert_eq!(en_rx_addr.erx_p(5), false);
 	assert_eq!(en_rx_addr.erx_p(7), false);
 	assert_eq!(
 		en_rx_addr.to_bools(),
-		[true, true, true, false, false, false]
+		[true, true, true, true, false, false]
 	);
+}
+
+#[test]
+fn registers() {
+	let mut config = Config::default();
+	assert_eq!(config.mask_rx_dr(), false);
+	assert_eq!(config.mask_tx_ds(), false);
+	assert_eq!(config.mask_max_rt(), false);
+	assert_eq!(config.en_crc(), true);
+	assert_eq!(config.crco(), false);
+	assert_eq!(config.pwr_up(), false);
+	assert_eq!(config.prim_rx(), false);
+	config.set_crco(true);
+	config.set_pwr_up(true);
+	assert_eq!(config.mask_rx_dr(), false);
+	assert_eq!(config.mask_tx_ds(), false);
+	assert_eq!(config.mask_max_rt(), false);
+	assert_eq!(config.en_crc(), true);
+	assert_eq!(config.crco(), true);
+	assert_eq!(config.pwr_up(), true);
+	assert_eq!(config.prim_rx(), false);
+
+	let mut en_aa = EnAa::default();
+	assert_eq!(en_aa.enaa_p(0), true);
+	assert_eq!(en_aa.enaa_p(1), true);
+	assert_eq!(en_aa.enaa_p(2), true);
+	assert_eq!(en_aa.enaa_p(3), true);
+	assert_eq!(en_aa.enaa_p(4), true);
+	assert_eq!(en_aa.enaa_p(5), true);
+	en_aa.set_enaa_p(0, false);
+	en_aa.set_enaa_p(5, true);
+	assert_eq!(en_aa.enaa_p(0), false);
+	assert_eq!(en_aa.enaa_p(1), true);
+	assert_eq!(en_aa.enaa_p(2), true);
+	assert_eq!(en_aa.enaa_p(3), true);
+	assert_eq!(en_aa.enaa_p(4), true);
+	assert_eq!(en_aa.enaa_p(5), true);
+
+	let mut en_rx_addr = EnRxaddr::default();
+	assert_eq!(en_rx_addr.erx_p(0), true);
+	assert_eq!(en_rx_addr.erx_p(1), true);
+	assert_eq!(en_rx_addr.erx_p(2), false);
+	assert_eq!(en_rx_addr.erx_p(3), false);
+	assert_eq!(en_rx_addr.erx_p(4), false);
+	assert_eq!(en_rx_addr.erx_p(5), false);
+	en_rx_addr.set_erx_p(0, false);
+	en_rx_addr.set_erx_p(5, true);
+	assert_eq!(en_rx_addr.erx_p(0), false);
+	assert_eq!(en_rx_addr.erx_p(1), true);
+	assert_eq!(en_rx_addr.erx_p(2), false);
+	assert_eq!(en_rx_addr.erx_p(3), false);
+	assert_eq!(en_rx_addr.erx_p(4), false);
+	assert_eq!(en_rx_addr.erx_p(5), true);
+
+
+	let mut setup_aw = SetupAw::default();
+	assert_eq!(setup_aw.aw(), 3);
+	setup_aw.set_aw(0);
+	assert_eq!(setup_aw.aw(), 0);
+
+	let mut setup_retr = SetupRetr::default();
+	assert_eq!(setup_retr.ard(), 0);
+	assert_eq!(setup_retr.arc(), 3);
+	setup_retr.set_ard(2);
+	setup_retr.set_arc(5);
+	assert_eq!(setup_retr.ard(), 2);
+	assert_eq!(setup_retr.arc(), 5);
+
+	let mut rf_ch = RfCh::default();
+	assert_eq!(rf_ch.rf_ch(), 2);
+	rf_ch.set_rf_ch(10);
+	assert_eq!(rf_ch.rf_ch(), 10);
+
+	let mut rf_setup = RfSetup::default();
+	assert_eq!(rf_setup.cont_wave(), false);
+	assert_eq!(rf_setup.rf_dr_low(), false);
+	assert_eq!(rf_setup.rf_dr_high(), true);
+	assert_eq!(rf_setup.rf_pwr(), 0b11);
+
+	let mut status = Status::default();
+	assert_eq!(status.rx_dr(), false);
+	assert_eq!(status.tx_ds(), false);
+	assert_eq!(status.max_rt(), false);
+	assert_eq!(status.rx_p_no(), 0b111);
+	assert_eq!(status.tx_full(), false);
+
+	let mut observe_tx = ObserveTx::default();
+	assert_eq!(observe_tx.plos_cnt(), 0);
+	assert_eq!(observe_tx.arc_cnt(), 0);
+
+	let mut rpd = RPD::default();
+	assert_eq!(rpd.rpd(), false);
+
+	let mut rx_pw5 = RxPwP5::default();
+	assert_eq!(rx_pw5.0, 0);
+
+	let mut fifo_status = FifoStatus::default();
+	assert_eq!(fifo_status.tx_reuse(), false);
+	assert_eq!(fifo_status.tx_full(), false);
+	assert_eq!(fifo_status.tx_empty(), true);
+	assert_eq!(fifo_status.rx_full(), false);
+	assert_eq!(fifo_status.rx_empty(), true);
+
+	let mut dynpd = Dynpd::default();
+	assert_eq!(dynpd.dpl_p(0), false);
+
+	let mut feature = Feature::default();
+	assert_eq!(feature.en_dpl(), false);
+	assert_eq!(feature.en_ack_pay(), false);
+	assert_eq!(feature.en_dyn_ack(), false);
+	feature.set_en_ack_pay(true);
+	assert_eq!(feature.en_dpl(), false);
+	assert_eq!(feature.en_ack_pay(), true);
+	assert_eq!(feature.en_dyn_ack(), false);
 }
